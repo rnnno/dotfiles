ec43f2d2aabe68fd9d5aafc98204f2ee8130c98d
rnnno <motoino11@gmail.com>
2024-02-08 14:51:38 +0900

    bufflineのUI変えた

--- config/nvim/lua/plugins/buffline.lua
+++ config/nvim/lua/plugins/buffline.lua
@@ -8,8 +8,36 @@ return {
   },
   config = function()
     require('bufferline').setup({
+      options = {
+        indicator = {
+          style = 'underline',
+        },
+
+        show_close_icon = false,
+        show_buffer_close_icons = false,
+        show_tab_indicators = false,
+        -- show_duplicate_prefix = true,
+
+        separator_style = { '', '' },
+
+
+        always_show_bufferline = false,
+
+        diagnostics = 'nvim_lsp',
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local s = " "
+          for e, n in pairs(diagnostics_dict) do
+            local sym = e == "error" and " "
+                or (e == "warning" and " " or "")
+            s = s .. n .. sym
+          end
+          return s
+        end
+      }
+
 
     })
+
     local keymap = vim.keymap.set
     keymap('n', '<A-,>', '<CMD>BufferLineCyclePrev<CR>')
     keymap('n', '<A-.>', '<CMD>BufferLineCycleNext<CR>')
