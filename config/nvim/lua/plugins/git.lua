8e14ea5d823bd81475ce80ee893b647775225cf2
rnnno <motoino11@gmail.com>
2024-02-19 17:25:56 +0900

    git系のプラグイン入れた

---
+++ config/nvim/lua/plugins/git.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'sindrets/diffview.nvim',
+    lazy = true,
+    event = 'BufReadPre',
+    opts = {},
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    lazy = true,
+    event = 'BufReadPre',
+    opts = {},
+  }
+}
