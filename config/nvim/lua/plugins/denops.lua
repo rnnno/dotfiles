0df6e3dbef6c74ff7cd7e6f587373021eebbea04
rnnno <motoino11@gmail.com>
2024-06-25 17:51:14 +0900

    create delete cache command

---
+++ config/nvim/lua/plugins/denops.lua
@@ -0,0 +1,11 @@
+return {
+  'vim-denops/denops.vim',
+  lazy = true,
+  cmd = { 'DenopsFixCache' },
+  config = function()
+    vim.cmd [[
+      " Fix Deno module cache issue
+      command! DenopsFixCache call denops#cache#update(#{reload: v:true})
+    ]]
+  end,
+}
