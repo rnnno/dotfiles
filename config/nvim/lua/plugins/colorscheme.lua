9f73e8125007d7a48c3b82ac681d153015d1faaf
rnnno <motochika.inoue@sun-asterisk.com>
2025-08-06 11:13:51 +0900

    update: colorscheme and lsp plugins

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -9,6 +9,28 @@ return {
 
     vim.cmd [[ colorscheme catppuccin]]
 
+    local transparent_groups = {
+      -- 'Normal',
+      'VertSplit',
+      'StatusLine',
+      'StatusLineNC',
+      'TabLine',
+      'TabLineFill',
+      'NormalFloat',
+      'NormalNC',
+      'NormalSB',
+      'FloatTitle',
+      'FloatFooter',
+      'TelescopeNormal',
+      'TelescopeSelectionCaret',
+    }
+    for _, group in ipairs(transparent_groups) do
+      vim.api.nvim_set_hl(0, group, { bg = 'NONE' })
+    end
+
+    -- なんかこうしないと白くなる
+    vim.cmd[[ highlight FloatBorder guibg=NONE ]]
+
     vim.api.nvim_set_hl(0, 'LineNr', { fg = '#fffacd' })
     vim.api.nvim_set_hl(0, 'Comment', { fg = '#a9a9a9' })
   end
