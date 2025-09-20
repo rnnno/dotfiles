1b1b652a9377f46eddfa8cfa6996dc2c72e76bb1
moto <moto@motos-MacBook-Air.local>
2025-09-21 06:11:40 +0900

    refactor: fix warns

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -16,7 +16,6 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "main",
     dependencies = {
-      { "github/copilot.vim" },
       { "nvim-lua/plenary.nvim" },
     },
     opts = {
