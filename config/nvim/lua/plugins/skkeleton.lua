4dcf173d7bc758f635c87cbd36fd42c4ce49393b
rnnno <motochika.inoue@sun-asterisk.com>
2026-02-18 16:42:04 +0900

    fix: switch skkeleton keymap to enable action

--- config/nvim/lua/plugins/skkeleton.lua
+++ config/nvim/lua/plugins/skkeleton.lua
@@ -8,10 +8,10 @@ return {
   config = function()
     local keymap = vim.keymap.set
     local opts = { silent = true, remap = true }
-    keymap('i', '<C-j>', '<Plug>(skkeleton-toggle)', opts)
-    keymap('c', '<C-j>', '<Plug>(skkeleton-toggle)', opts)
-    keymap('t', '<C-j>', '<plug>(skkeleton-toggle)', opts)
-    keymap('n', '<C-j>', 'i<plug>(skkeleton-toggle)', opts)
+    keymap('i', '<C-j>', '<Plug>(skkeleton-enable)', opts)
+    keymap('c', '<C-j>', '<Plug>(skkeleton-enable)', opts)
+    keymap('t', '<C-j>', '<plug>(skkeleton-enable)', opts)
+    keymap('n', '<C-j>', 'i<plug>(skkeleton-enable)', opts)
 
     local dictionaries = {}
     local handle = io.popen('ls $HOME/.skk/*')
@@ -28,25 +28,6 @@ return {
       globalDictionaries = dictionaries,
       keepState = false,
     })
-
-    local augroup = vim.api.nvim_create_augroup
-    local autocmd = vim.api.nvim_create_autocmd
-
-    augroup('skkeletonKeepState', { clear = true })
-    autocmd('BufEnter', {
-      group = 'skkeletonKeepState',
-      pattern = '*',
-      callback = function()
-        vim.fn['skkeleton#config']({ keepState = false })
-      end
-    })
-    -- autocmd('BufEnter', {
-    --   group = 'skkeletonKeepState',
-    --   pattern = { '*.txt', '*.md' },
-    --   callback = function()
-    --     vim.fn['skkeleton#config']({ keepState = true })
-    --   end
-    -- })
 
     vim.fn['skkeleton#register_kanatable'](
       'rom',
